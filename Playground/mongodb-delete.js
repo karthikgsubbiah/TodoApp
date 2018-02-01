5c70c6296b97c598e5662c73a5489db8d9365076
karthikgsubbiah <karthik.g.subbiah@gmail.com>
2018-02-01 19:02:06 +0530

    Delete operation file updated

--- Playground/mongodb-delete.js
+++ Playground/mongodb-delete.js
@@ -6,5 +6,26 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB Server');
 
+  //Deletes all the documemnts which satisfies the particular condition
+  db.collection('Users').deleteMany({name : 'Karthik'}).then((result) => {
+    console.log(result);
+  }, (err) => {
+      console.log('Document not deleted', err);
+  });
+
+//Deletes one of the documemnts which satisfies the particular condition
+  db.collection('Users').deleteOne({age: 19}).then((result) => {
+    console.log(result);
+  }, (err) => {
+    console.log('Document not deleted', err);
+  });
+
+  //Deletes one of the documemnts which satisfies the particular condition and returns the deleted document for display
+  db.collection('Todos').findOneAndDelete({text: 'Something to do'}).then((result) => {
+    console.log(result);
+  }, (err) => {
+    console.log('Document not deleted', err);
+  });
+
   db.close();
 })
