054603271e376811dc4c027c01f8688bd5f5452e
karthikgsubbiah <karthik.g.subbiah@gmail.com>
2018-02-03 19:07:09 +0530

    Added GET /todos and a test suite for POST /todos

--- Server/server.js
+++ Server/server.js
@@ -21,6 +21,18 @@ app.post('/todos', (req, res) => {
   })
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (err) => {
+    res.send(err);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.express = {
+  app
+}
